62d7fc8589ce983c98d77cd68cc87c59f279a8fd
yescine <bouchoucha.yassine@gmail.com>
2024-11-14 06:47:48 +0000

    all c# basic syntax

--- ConsoleAppHello/Program.cs
+++ ConsoleAppHello/Program.cs
@@ -5,9 +5,11 @@ using ConsoleAppHello;
 
 new Initializations().greet();
 
+int x = (int)1.5;
+int y = 2;
 // ! Could be used without namespace import
 var engine = new MathsCalc();
-Points origin = new(1, 2);
+Points origin = new(x, y);
 var result = engine.Increment(origin.X);
 
 Console.WriteLine("the calculation result is: {0}", result);
@@ -19,3 +21,6 @@ Console.WriteLine("the calculation result is: {0}", result);
 //     new Initializations().greet();
 //   }
 // }
+
+var syntaxInit = new Syntax("names");
+syntaxInit.Linq();
---
+++ ConsoleAppHello/Syntax.cs
@@ -0,0 +1,40 @@
+﻿// namespace SyntaxSpace;
+
+public class Syntax(string name, int? batch = null)
+{
+  public string Name { get; } = name.Replace("s", "");
+  public bool IsPlural { get; } = name.Contains('s');
+  public int Batch { get; } = batch ?? 5; // Set default value of 5 if batch is null
+
+  private List<char> Process { get; } = ['A', 'C', 'R', 'X', 'Y'];
+
+  public bool TestLoop()
+  {
+    var result = true;
+    bool cond = Process.Count > 2 && IsPlural;
+    if (cond) result = !IsPlural;
+    return result;
+  }
+
+  public void PrintBatch()
+  {
+    Console.WriteLine($"last index: {Process[^1]}");
+    foreach (var idx in Process[1..3])
+    {
+      Console.WriteLine($"idx: {idx}");
+    }
+  }
+
+  public void Linq()
+  {
+    int[] scores = { 97, 92, 81, 60, 44, 99, 20, 120 };
+    var minScore = 80;
+    IEnumerable<int> scoreQuery =
+      from score in scores
+      where score > minScore && score < 100
+      orderby score ascending
+      select score;
+    Console.WriteLine($"\nLinq list {string.Join(" | ", scoreQuery)} with total ");
+  }
+
+}
