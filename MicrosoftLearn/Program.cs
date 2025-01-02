0d3339011582f3584c08398293a192a965950063
yescine <bouchoucha.yassine@gmail.com>
2025-01-02 16:21:01 +0000

    Basic class implementation

---
+++ MicrosoftLearn/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// -- Basic Class manipulation 
+Random dice = new Random(); //? Statefull classes
+int roll = dice.Next(1, 7);
+
+Console.WriteLine($"Rolling... {roll}");
+
